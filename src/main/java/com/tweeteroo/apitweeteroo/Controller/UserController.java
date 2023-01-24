d123ff76d7d1f0ceeff3e07db548f2c4b969cd75
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-24 14:00:34 -0300

    fix: update  HttpStatus for OK

--- src/main/java/com/tweeteroo/apitweeteroo/Controller/UserController.java
+++ src/main/java/com/tweeteroo/apitweeteroo/Controller/UserController.java
@@ -1,9 +1,11 @@
 package com.tweeteroo.apitweeteroo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweeteroo.apitweeteroo.Dto.UserDTO;
@@ -16,6 +18,7 @@ public class UserController {
   @Autowired
   private UserService service;
 
+  @ResponseStatus(code = HttpStatus.OK) 
   @PostMapping
     public void create(@RequestBody UserDTO req) {
     service.save(req);
